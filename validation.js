7c012588d4104e0c3ba65172e19eb8b2b38880e4
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-04-14 14:14:07 +0000

    Solve ES6 linters error

--- validation.js
+++ validation.js
@@ -1,23 +1,22 @@
 const email = document.getElementById('email');
-const emailInput = document.querySelector("small");
+const emailInput = document.querySelector('small');
 
 function validateLowerChar() {
   const EMAIL_REGEX_L = /^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/;
 
   if (email.value === '') {
-    emailInput.style.display = "none";
+    emailInput.style.display = 'none';
   }
-  else {
-    if (!EMAIL_REGEX_L.test(email.value)) {
-      emailInput.innerHTML = 'Please enter valid email in lowercase';
-      emailInput.style.display = "block";
-    }
-    else {
-      //Do nothing
-    }
+  if (!EMAIL_REGEX_L.test(email.value)) {
+    emailInput.innerHTML = 'Please enter valid email in lowercase';
+    emailInput.style.display = 'block';
   }
 }
 
+validateLowerChar();
+
 function dismiss() {
-  emailInput.style.display = "none";
+  emailInput.style.display = 'none';
 }
+
+dismiss();
