c44748f0c4cc7f7cf9f1328a71c6cef20c209f12
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-04-15 10:39:03 +0000

    Modify JS file for contact form

--- validation.js
+++ validation.js
@@ -1,22 +1,15 @@
 const email = document.getElementById('email');
-const emailInput = document.querySelector('small');
+const emailMessage = document.getElementById('emailMessage');
+const emailREGEX = /^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/;
 
-function validateLowerChar() {
-  const EMAIL_REGEX_L = /^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/;
-
-  if (email.value === '') {
-    emailInput.style.display = 'none';
+function myFunc() {
+  if (emailREGEX.test(email.value)) {
+    emailMessage.innerHTML = 'Email is correct';
+    emailMessage.style.display = 'block';
   }
-  if (!EMAIL_REGEX_L.test(email.value)) {
-    emailInput.innerHTML = 'Please enter valid email in lowercase';
-    emailInput.style.display = 'block';
+  else {
+    emailMessage.innerHTML = 'Please enter valid email in lowercase';
+    emailMessage.style.display = 'block';
+    return false;
   }
 }
-
-validateLowerChar();
-
-function dismiss() {
-  emailInput.style.display = 'none';
-}
-
-dismiss();
