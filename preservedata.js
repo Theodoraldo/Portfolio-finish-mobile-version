67f5336d292807d198c1d9ec166c34fb4c3cedfe
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-01 05:31:13 +0000

    Add functionality to data preservation

---
+++ preservedata.js
@@ -0,0 +1,27 @@
+const sName = document.getElementById('name');
+const sEmail = document.getElementById('email');
+const sMessage = document.getElementById('message');
+
+let saveName = () => {
+  const inputData = {
+    userName: sName.value,
+    userEmail: sEmail.value,
+    userMessage: sMessage.value,
+  };
+  localStorage.setItem('InputData', JSON.stringify(inputData));
+}
+
+let dataRetrieve = () => {
+  let data = JSON.parse(localStorage.getItem('InputData'));
+  if (data) {
+    sName.value = data.userName;
+    sEmail.value = data.userEmail;
+    sMessage.value = data.userMessage;
+  }
+}
+
+sName.addEventListener('change', saveName);
+sEmail.addEventListener('change', saveName);
+sMessage.addEventListener('change', saveName);
+
+window.document.onload = dataRetrieve();
